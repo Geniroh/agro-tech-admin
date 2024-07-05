0fbb0b0e5e8636bdc7f62cfde519874ed7a02539
Geniroh <irochibuzor@gmail.com>
2024-07-05 03:01:26 +0100

    feature 1 deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import dotenv from "dotenv";
+import react from "@vitejs/plugin-react";
 
+dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    "process.env": process.env,
+  },
+  base: "/",
   plugins: [react()],
-})
+  preview: {
+    port: 3000,
+    strictPort: false,
+  },
+  server: {
+    port: 3000,
+    strictPort: false,
+    host: true,
+    origin: `http://0.0.0.0:${process.env.PORT}`,
+  },
+});
